5ab093e53d93f59db7089f2bdfa6f6a9f2ec243b
andarel666 <113385366+andarel666@users.noreply.github.com>
2022-12-14 14:46:21 -0600

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-  def scriptcontents = libraryResource "test/${config.name}"
+  def scriptcontents = libraryResource "scripts/${config.name}"
   writeFile file: "${config.name}", text: scriptcontents
   sh "chmod a+x ./${config.name}"
 }
